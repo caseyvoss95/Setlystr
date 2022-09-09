12bccc8bd07445bb2b97ee3fd4393e5d86913ead
admin <admin@admins-MBP.cable.rcn.com>
2022-09-09 13:36:37 -0400

    router running

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const mongoose = require("mongoose");
 const methodOverride = require('method-override')
 
 const app = express();
+const setlistController = require('./controllers/setlist.js');
+
 require("dotenv").config(); 
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use('/setlist', setlistController);
 
 //listener
 const PORT = process.env.PORT;
 app.listen(PORT, ()=> {
     console.log("setlystr running");
-})
+});
